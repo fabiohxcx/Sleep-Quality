8bb4f393486077f8b05414b4e5b37d5bcefeef65
Fabio Lagoa <fabio.lagoa@bv.com.br>
2019-08-21 16:35:26 -0300

    recording sleep Quality

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
@@ -18,6 +18,7 @@ package com.example.android.trackmysleepquality.sleeptracker
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import com.example.android.trackmysleepquality.database.SleepDatabaseDao
@@ -44,6 +45,11 @@ class SleepTrackerViewModel(
     private var tonight = MutableLiveData<SleepNight?>()
 
     private val nights = database.getAllNights()
+
+    private val _navigateToSleepQuality = MutableLiveData<SleepNight>()
+    val navigateToSleepQuality: LiveData<SleepNight>
+        get() = _navigateToSleepQuality
+
 
     init {
         initializeTonight()
@@ -86,6 +92,8 @@ class SleepTrackerViewModel(
             oldNight.endTimeMilli = System.currentTimeMillis()
             update(oldNight)
 
+            _navigateToSleepQuality.value = oldNight
+
         }
     }
 
@@ -108,9 +116,12 @@ class SleepTrackerViewModel(
         }
     }
 
-    val nightString = Transformations.map(nights) {
-        nights ->
+    val nightString = Transformations.map(nights) { nights ->
         formatNights(nights, application.resources)
+    }
+
+    fun doneNavigating() {
+        _navigateToSleepQuality.value = null
     }
 
 }
